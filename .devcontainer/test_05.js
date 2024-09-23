6754171c4ec731849d9463c957fc21633dd497da
Subhasis Behera <146092322+subhasis-behera001@users.noreply.github.com>
2024-09-23 18:03:49 +0000

    operations

--- .devcontainer/test_05.js
+++ .devcontainer/test_05.js
@@ -6,3 +6,11 @@ console.log("6"+9+9)
 
 
 //relational operater(>,<,>=,<=,==,!=)
+/*
+Difference between == and ===
+==                                      ===
+it compares the value only              it compares the value as well as the datatype
+in this type coercion happens           in this tyoe coversion did not happens
+it is normal comparision operator       it is strictly comparision operator
+
+*/
