a79311d4eb1f78f272ff9bc441b48355fba618ba
Subhasis Behera <146092322+subhasis-behera001@users.noreply.github.com>
2024-09-23 17:20:19 +0000

    datatype

---
+++ .devcontainer/test_03.js
@@ -0,0 +1,13 @@
+//"use strict"->treat all code as newer version.
+//alert(5+8)//WE are using node.js not browser.
+/*
+number (2 to the power 53)
+bigint 
+string->""
+boolean
+undefined->declaring variable without assigning any value
+null->stand alone value(null is an object)
+symbol->used in react(when we have to go for unique)
+object
+..typeof var.name
+ */
