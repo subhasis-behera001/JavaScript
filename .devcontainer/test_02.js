7be2125369ba8208ec6ed23b3366f418fa781f92
Subhasis Behera <146092322+subhasis-behera001@users.noreply.github.com>
2024-09-23 17:05:45 +0000

    variables

---
+++ .devcontainer/test_02.js
@@ -0,0 +1,10 @@
+var pass="pbsppb"
+const acc=1
+let email="subhasisbehera@gmail.com"
+console.log(acc);
+console.log([[pass,acc,email]])
+console.table([pass,acc,email])
+/*
+for varibles use let
+for constant use const
+*/
