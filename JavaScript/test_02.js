b967cc19fbc40ebb4a1750e3aecbda6c45f70e0c
Subhasis Behera <146092322+subhasis-behera001@users.noreply.github.com>
2024-10-01 18:11:16 +0000

    math and number

---
+++ JavaScript/test_02.js
@@ -0,0 +1,14 @@
+//------------NUMBER------------------->
+let num=100
+console.log(num.toString().length)//3
+console.log(num.toFixed(1))//100.0
+ 
+const num1=234.33332423
+ console.log(num1.toPrecision(4))//234.3
+ //----------------------Math------------------>
+ let num2=49
+ console.log(Math.sqrt(num2))
+ //Math.random()
+ let min=1
+ let max=6
+ console.log(Math.floor(Math.random()*(max-min +1)))
