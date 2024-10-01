7e47d11610d898ee2352f0b0a517b78cfe85b00a
Subhasis Behera <146092322+subhasis-behera001@users.noreply.github.com>
2024-10-01 17:48:30 +0000

    String

---
+++ JavaScript/test_01.js
@@ -0,0 +1,20 @@
+//String
+let count =10
+console.log(`The count is ${count}`);
+
+
+let str=new String('love')
+console.log(str)
+console.log(str.length)
+console.log(str[0])
+//------------------------METHODS------------------------------------>
+/**
+ * CharAt(index)
+ * indexof(character)
+ * substring(1st index ,last index)
+ * trim()---->remove starting and ending spaces
+ * slice()
+ * replace('old str','new str')
+ * includes('str')
+ * split(based on)
+ */
