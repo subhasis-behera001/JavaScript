52f9e787dfeea8605732580ce05ba640be5546db
Subhasis Behera <146092322+subhasis-behera001@users.noreply.github.com>
2024-10-05 00:51:07 +0000

    de-structure

---
+++ Object/test_03.js
@@ -0,0 +1,11 @@
+//de-structure
+const student = {
+    name:"subhasis",
+    age:22,
+    gender:"male",
+    regNo:1
+}
+// const {name4}= student
+// console.log(name4)
+let {name: n1}=student
+console.log(n1)
