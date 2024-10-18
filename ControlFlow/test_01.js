1f61af56554dc0e2a7462acfab2561eda955c83d
Subhasis Behera <146092322+subhasis-behera001@users.noreply.github.com>
2024-10-18 22:31:24 +0000

    control flow

---
+++ ControlFlow/test_01.js
@@ -0,0 +1,16 @@
+if(condition){
+    //contex
+}
+/**
+ * if 
+ * if else 
+ * if else ladder 
+ * switch
+ * 
+ * 
+ * Nullish Coalescing Operator(??)
+ * 
+ * let val;
+ * val =null ?? 20;
+ * console.log(val)//20
+ */
