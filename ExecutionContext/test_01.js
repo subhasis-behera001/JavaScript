fa7b30b58d7505ad7c9d168733a5101d9d5257e2
Subhasis Behera <146092322+subhasis-behera001@users.noreply.github.com>
2024-10-18 22:32:04 +0000

    Execution context

---
+++ ExecutionContext/test_01.js
@@ -0,0 +1,16 @@
+// javascritp execution context
+
+
+/**
+ * {}--> global execution context(this)
+ *      function execution context
+ *      eval execution context
+ * 
+ * Phase
+ * {}==> 2.memory creation phase
+ *      3.execution phase
+ * 
+ *     video -25 
+ * 
+ *      call stack  
+ */
