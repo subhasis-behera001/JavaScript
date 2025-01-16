304350490314f19a2fab822baf5580ed0901a999
Subhasis Behera <146092322+subhasis-behera001@users.noreply.github.com>
2025-01-16 13:46:26 +0000

    while and do while loop

---
+++ loops/test_02.js
@@ -0,0 +1,12 @@
+//WHILE LOOP
+let i=0;
+let arr=[0,1,2,3,4,4,4]
+// while(i<=arr.length)
+// {
+//     console.log(arr[i])
+//     i++;
+// }
+do{
+    console.log(arr[i])
+    i++;
+}while(i<=arr.length);
